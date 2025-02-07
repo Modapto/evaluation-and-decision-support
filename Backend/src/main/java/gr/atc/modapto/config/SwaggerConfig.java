7271c90a9e197ce0c61fe6a94b21a0a601496ae3
AlkAzna <a.aznavouridis@atc.gr>
2025-02-07 16:42:45 +0200

    Updated Version, OpenAPI and CI/CD

--- Backend/src/main/java/gr/atc/modapto/config/SwaggerConfig.java
+++ Backend/src/main/java/gr/atc/modapto/config/SwaggerConfig.java
@@ -2,20 +2,24 @@ package gr.atc.modapto.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.SpecVersion;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${build.version}")
+    private String appVersion;
+
     @Bean
     public OpenAPI openAPIDocumentation() {
         return new OpenAPI()
-                .specVersion(SpecVersion.V30)
                 .info(new Info()
                     .title("Evaluation and Decision Support API")
-                    .version("1.0")
-                    .description("API documentation for EDS Service of MODAPTO project"));
+                    .version(appVersion)
+                    .description("API documentation for EDS Service of MODAPTO project"))
+                .openapi("3.0.3");
     }
 }
