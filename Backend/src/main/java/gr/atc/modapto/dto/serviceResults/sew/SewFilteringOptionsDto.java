a9a5410fa37074ba2ba49f0b13fb8e51d2e1306e
AlkAzna <a.aznavouridis@atc.gr>
2025-09-30 12:20:52 +0300

    Added format of Date

--- Backend/src/main/java/gr/atc/modapto/dto/serviceResults/sew/SewFilteringOptionsDto.java
+++ Backend/src/main/java/gr/atc/modapto/dto/serviceResults/sew/SewFilteringOptionsDto.java
@@ -1,14 +1,19 @@
 package gr.atc.modapto.dto.serviceResults.sew;
 
-import com.fasterxml.jackson.annotation.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -42,6 +47,11 @@ public class SewFilteringOptionsDto {
         @JsonProperty("date")
         @JsonAlias({"Date", "date"})
         @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+         @Schema(
+            description = "The date in format dd-MM-yyyy HH:mm:ss",
+            type = "string",
+            example = "30-09-2025 14:35:00"
+        )
         private LocalDateTime date;
     }
 }
