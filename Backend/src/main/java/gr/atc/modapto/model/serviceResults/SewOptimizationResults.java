b3c254edfa6b07c50536c54984fae23d7bf1219d
AlkAzna <a.aznavouridis@atc.gr>
2025-10-16 15:52:45 +0300

    Disable dynamic key mapping

--- Backend/src/main/java/gr/atc/modapto/model/serviceResults/SewOptimizationResults.java
+++ Backend/src/main/java/gr/atc/modapto/model/serviceResults/SewOptimizationResults.java
@@ -1,15 +1,16 @@
 package gr.atc.modapto.model.serviceResults;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.util.List;
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -26,7 +27,7 @@ public class SewOptimizationResults {
     @Field(name = "moduleId", type = FieldType.Keyword)
     private String moduleId;
 
-    @Field(name = "data", type = FieldType.Object)
+    @Field(name = "data", type = FieldType.Object, enabled = false)
     private Map<String, SolutionData> data;
 
     @Data
@@ -34,16 +35,16 @@ public class SewOptimizationResults {
     @AllArgsConstructor
     public static class SolutionData {
 
-        @Field(type = FieldType.Object)
+        @Field(type = FieldType.Object, enabled = false)
         private MetricsData metrics;
 
-        @Field(type = FieldType.Object)
+        @Field(type = FieldType.Object, enabled = false)
         private Map<String, Map<String, String>> seq;
 
-        @Field(type = FieldType.Object)
+        @Field(type = FieldType.Object, enabled = false)
         private Map<String, Map<String, Map<String, Map<String, TimeRange>>>> orders;
 
-        @Field(name = "init_order", type = FieldType.Object)
+        @Field(name = "init_order", type = FieldType.Object, enabled = false)
         private List<String> initOrder;
     }
 
